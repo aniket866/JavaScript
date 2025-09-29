fe63ab27ce4e188b88f63035e2045c6e0db5e01b
aniket866 <iamaniketkumarmaner@gmail.com>
2025-09-30 02:33:34 +0530

    Add Morris Inorder Traversal algorithm with tests

---
+++ Trees/MorrisTraversal.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+
+  morrisTraversal() {
+    const traversal = []
+    let current = this.root
+
+    while (current !== null) {
+      if (current.left === null) {
+        traversal.push(current.data)
+        current = current.right
+      } else {
+        let predecessor = current.left
+        while (predecessor.right !== null && predecessor.right !== current) {
+          predecessor = predecessor.right
+        }
+
+        if (predecessor.right === null) {
+          predecessor.right = current
+          current = current.left
+        } else {
+          predecessor.right = null
+          traversal.push(current.data)
+          current = current.right
+        }
+      }
+    }
+
+    return traversal
+  }
+}
+
+module.exports = { BinaryTree, Node }
---
+++ Trees/test/MorrisTraversal.test.js
@@ -0,0 +1,23 @@
+const { BinaryTree, Node } = require('../MorrisTraversal')
+
+describe('Morris Inorder Tree Traversal', () => {
+  const binaryTree = new BinaryTree()
+
+  const root = new Node(7)
+  root.left = new Node(5)
+  root.right = new Node(8)
+  root.left.left = new Node(3)
+  root.left.right = new Node(6)
+  root.left.right.right = new Node(9)
+  binaryTree.root = root
+
+  it('Binary tree - Empty case', () => {
+    const emptyTree = new BinaryTree()
+    expect(emptyTree.morrisTraversal()).toStrictEqual([])
+  })
+
+  it('Binary tree - Morris inorder traversal', () => {
+    const traversal = binaryTree.morrisTraversal()
+    expect(traversal).toStrictEqual([3, 5, 6, 9, 7, 8])
+  })
+})
